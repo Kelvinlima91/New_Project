adb157deb7638c946c3af939aad47928e1f9e9eb
Kelvinlima91 <116626977+Kelvinlima91@users.noreply.github.com>
2026-03-09 23:37:11 -0100

    Update protocolcounter.php

--- LroOnline/app/Models/ProtocolCounter.php
+++ LroOnline/app/Models/ProtocolCounter.php
@@ -1,10 +1,25 @@
 <?php
+// app/Models/ProtocolCounter.php (NOT Protocol_Counters.php)
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Protocol_Counters extends Model
+class ProtocolCounter extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'protocol_counters';
+
+    protected $fillable = [
+        'year',
+        'island',
+        'last_number',
+    ];
+
+    protected $casts = [
+        'year' => 'integer',
+        'last_number' => 'integer',
+    ];
 }
